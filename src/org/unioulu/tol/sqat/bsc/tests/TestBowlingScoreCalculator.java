0752a7d7cfec15cd15da2889cb796149ee8c5eaf
somename <someemail>
2015-10-16 11:48:00 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -9,22 +9,23 @@ import org.unioulu.tol.sqat.bsc.Frame;
 public class TestBowlingScoreCalculator {
 	@Test
 	public void test() {
-		Frame f1 = new Frame(2,2);
+		Frame f1 = new Frame(2,2); //4
 		// 1. & 2.		
 		assertEquals(f1.score(),4);
 		
 		//3.
-		Frame f2 = new Frame(2,2);
-		Frame f3 = new Frame(1,4);
-		Frame f4 = new Frame(2,6);
-		Frame f5 = new Frame(5,2);
-		Frame f6 = new Frame(3,5);
-		Frame f7 = new Frame(4,4);
-		Frame f8 = new Frame(3,6);
-		Frame f9 = new Frame(1,8);
-		Frame f10 = new Frame(2,7);
+		Frame f2 = new Frame(2,2); // 8
+		Frame f3 = new Frame(1,4); // 13
+		Frame f4 = new Frame(2,6); // 21
+		Frame f5 = new Frame(5,2); // 28
+		Frame f6 = new Frame(3,5); // 36
+		Frame f7 = new Frame(4,4); // 44
+		Frame f8 = new Frame(3,6); // 53
+		Frame f9 = new Frame(1,8); // 62
+		Frame f10 = new Frame(2,7); // 71
 		
 		BowlingGame bG1 = new BowlingGame();
+		bG1.addFrame(f1);
 		bG1.addFrame(f2);
 		bG1.addFrame(f3);
 		bG1.addFrame(f4);
@@ -34,7 +35,7 @@ public class TestBowlingScoreCalculator {
 		bG1.addFrame(f8);
 		bG1.addFrame(f9);
 		bG1.addFrame(f10);
-		assertEquals(bG1.score(),4);
+		assertEquals(bG1.score(),71);
 
 		
 		
